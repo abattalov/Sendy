219bc0107927be10434215abf4d7bdadf7155f4c
github username <github email address>
2022-04-06 23:11:56 -0500

    added scorecard index and show pages

--- app/controllers/scorecards_controller.rb
+++ app/controllers/scorecards_controller.rb
@@ -1,6 +1,10 @@
 class ScorecardsController < ApplicationController
 
     def index
-        
+        @scorecards = Scorecard.all
+    end
+
+    def show
+        @scorecard = Scorecard.find_by(id: params[:id])
     end
 end
