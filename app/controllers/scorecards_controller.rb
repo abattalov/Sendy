f35c1eb48d34b436279a6e041a064ede480124db
github username <github email address>
2022-04-07 23:39:49 -0500

    added controller methods, new and edit forms for scorecards

--- app/controllers/scorecards_controller.rb
+++ app/controllers/scorecards_controller.rb
@@ -7,4 +7,31 @@ class ScorecardsController < ApplicationController
     def show
         @scorecard = Scorecard.find_by(id: params[:id])
     end
+
+    def new
+        @scorecard = Scorecard.new
+    end
+
+    def edit
+        @scorecard = Scorecard.find_by(id: params[:id])
+    end
+
+    def update
+
+    end
+
+    def destroy
+
+    end
+
+    private
+
+    def scorecard_params(array_args)
+        params.require(:scorecard).permit(array_args)
+    end
+
+    def params_array
+        [:course, :dates, :strokes, :score]
+    end
+
 end
